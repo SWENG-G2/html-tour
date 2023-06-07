586232e6008ddec0ae40aa0e27ae0db8f77bc57d
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-06-07 12:38:36 +0100

    Build website
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

---
+++ _astro/hoisted.8d112549.js
@@ -0,0 +1,1 @@
+import"./hoisted.c90146e0.js";const e=document.getElementById("scrollBtn");e&&e.addEventListener("click",()=>{const t=document.getElementById("content");t&&t.scrollIntoView({behavior:"smooth"})});
