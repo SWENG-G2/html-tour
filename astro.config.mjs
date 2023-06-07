e1155a0ad74544f1048f65014afd1b5d6002e8d1
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-06-07 12:41:39 +0100

    Astro config for pages deployment
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  site: 'https://:sweng-g2.github.io',
+  base: '/html-tour'
 });
