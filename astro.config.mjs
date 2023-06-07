8b3749bde669a653d12efa39e9c7c83581919d3e
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-06-07 12:42:31 +0100

    Fix typo in site url
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://:sweng-g2.github.io',
+  site: 'https://sweng-g2.github.io',
   base: '/html-tour'
 });
